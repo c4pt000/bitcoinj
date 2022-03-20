02f736104edf1b7743a5a768ea25b6ad34298e72
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2022-03-19 18:52:38 -0700

    Update LegacyAddress.java

--- core/src/main/java/org/bitcoinj/core/LegacyAddress.java
+++ core/src/main/java/org/bitcoinj/core/LegacyAddress.java
@@ -153,13 +153,13 @@ public class LegacyAddress extends Address {
                 else if (version == p.getP2SHHeader())
                     return new LegacyAddress(p, true, bytes);
             }
-            throw new AddressFormatException.InvalidPrefix("No network found for " + base58);
+          //  throw new AddressFormatException.InvalidPrefix("No network found for " + base58);
         } else {
             if (version == params.getAddressHeader())
                 return new LegacyAddress(params, false, bytes);
             else if (version == params.getP2SHHeader())
                 return new LegacyAddress(params, true, bytes);
-            throw new AddressFormatException.WrongNetwork(version);
+        //    throw new AddressFormatException.WrongNetwork(version);
         }
     }
 
