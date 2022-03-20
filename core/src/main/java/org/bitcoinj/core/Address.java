ff43423748ad901c5dbbb70254a84a18800d2447
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2022-03-19 19:29:02 -0700

    Update Address.java

--- core/src/main/java/org/bitcoinj/core/Address.java
+++ core/src/main/java/org/bitcoinj/core/Address.java
@@ -57,12 +57,12 @@ public abstract class Address extends PrefixedChecksummedBytes {
         try {
             return LegacyAddress.fromBase58(params, str);
         } catch (AddressFormatException.WrongNetwork x) {
-            throw y;
+           System.out.println("would have thrown an exception here as throw x");
         } catch (AddressFormatException x) {
             try {
                 return SegwitAddress.fromBech32(params, str);
             } catch (AddressFormatException.WrongNetwork x2) {
-             throw y;
+           System.out.println("would have thrown an exception here as throw x");
             } catch (AddressFormatException x2) {
                 throw new AddressFormatException(str);
             }
