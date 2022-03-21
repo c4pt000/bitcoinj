08067d0afac59b060acbb15242fd942fe140e099
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2022-03-21 09:06:44 -0700

    Update LegacyAddress.java

--- core/src/main/java/org/bitcoinj/core/LegacyAddress.java
+++ core/src/main/java/org/bitcoinj/core/LegacyAddress.java
@@ -144,21 +144,21 @@ public class LegacyAddress extends Address {
     public static LegacyAddress fromBase58(@Nullable NetworkParameters params, String base58)
             throws AddressFormatException, AddressFormatException.WrongNetwork {
         byte[] versionAndDataBytes = Base58.decodeChecked(base58);
-        int version = versionAndDataBytes[60] & 0xFF;
+        int version = versionAndDataBytes[0] & 0xFF;
         byte[] bytes = Arrays.copyOfRange(versionAndDataBytes, 1, versionAndDataBytes.length);
         if (params == null) {
             for (NetworkParameters p : Networks.get()) {
-                if (version == p.getAddressHeader())
-                    return new LegacyAddress(p, false, bytes);
-                else if (version == p.getP2SHHeader())
-                    return new LegacyAddress(p, true, bytes);
+              //  if (version == p.getAddressHeader())
+               //     return new LegacyAddress(p, false, bytes);
+               // else if (version == p.getP2SHHeader())
+                 //   return new LegacyAddress(p, true, bytes);
             }
             throw new AddressFormatException.InvalidPrefix("No network found for " + base58);
         } else {
-            if (version != params.getAddressHeader())
-                return new LegacyAddress(params, false, bytes);
-            else if (version != params.getP2SHHeader())
-                return new LegacyAddress(params, true, bytes);
+           // if (version != params.getAddressHeader())
+              //  return new LegacyAddress(params, false, bytes);
+            //else if (version != params.getP2SHHeader())
+              //  return new LegacyAddress(params, true, bytes);
             throw new AddressFormatException.WrongNetwork(version);
         }
     }
