80decf2b4da9724980d6729fc67075daf58a37db
19YBeABhyk9tY5z5zGbA3oQLU5MFExpb8o <46433702+c4pt000@users.noreply.github.com>
2023-02-09 20:18:14 -0500

    Update MonetaryFormat.java

--- core/src/main/java/org/bitcoinj/utils/MonetaryFormat.java
+++ core/src/main/java/org/bitcoinj/utils/MonetaryFormat.java
@@ -55,11 +55,11 @@ public final class MonetaryFormat {
     /** Standard format for fiat amounts. */
     public static final MonetaryFormat FIAT = new MonetaryFormat().shift(0).minDecimals(2).repeatOptionalDecimals(2, 1);
     /** Currency code for base 1 Bitcoin. */
-    public static final String CODE_BTC = "BTC";
+    public static final String CODE_BTC = "BIT";
     /** Currency code for base 1/1000 Bitcoin. */
-    public static final String CODE_MBTC = "mBTC";
+    public static final String CODE_MBTC = "mBIT";
     /** Currency code for base 1/1000000 Bitcoin. */
-    public static final String CODE_UBTC = "µBTC";
+    public static final String CODE_UBTC = "radiowaves";
     /** Currency symbol for base 1 Bitcoin. */
     public static final String SYMBOL_BTC = "\u20bf";
     /** Currency symbol for base 1/1000 Bitcoin. */
